72b8f8b89c429656ca72144588d48058076c00a2
Igor <izholobov2004@gmail.com>
2024-04-16 13:09:40 +0300

    Lab 19

--- Lab_19/src/test/java/ru/mirea/lab19/Lab19ApplicationTests.java
+++ Lab_19/src/test/java/ru/mirea/lab19/Lab19ApplicationTests.java
@@ -1,10 +1,10 @@
-package ru.mirea.lab18;
+package ru.mirea.lab19;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab16ApplicationTests {
+class Lab19ApplicationTests {
 
 	@Test
 	void contextLoads() {
