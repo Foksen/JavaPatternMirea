6cbc0d09682d63298551da4e43c6de4e7b6b69a1
Igor <izholobov2004@gmail.com>
2024-04-16 13:49:17 +0300

    Lab 20

--- Lab_20/src/test/java/ru/mirea/lab20/Lab20ApplicationTests.java
+++ Lab_20/src/test/java/ru/mirea/lab20/Lab20ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab19ApplicationTests {
+class lab20ApplicationTests {
 
 	@Test
 	void contextLoads() {
